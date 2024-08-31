09bd1438929c345aa2c08252451742edeaa5baf9
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-31 15:52:38 +0530

    mapping spec

--- src/main/java/com/example/springmvc/controller/demo.java
+++ src/main/java/com/example/springmvc/controller/demo.java
@@ -1,15 +1,22 @@
 package com.example.springmvc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("demo")
+@RequestMapping("nishan")
 public class demo{
     @GetMapping
     public String healthTest(){
         return "healthTest";
     }
+    @GetMapping("other")
+    public String get(){
+        return "get";
+    }
+    @PostMapping
+    public String update(){return "update";}
 
 
 }
