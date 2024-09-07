300f4ab1a23cd2294a8cad885bd620fc83406e18
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-07 09:24:42 +0530

    mapping spec

--- src/main/java/com/example/springmvc/controller/Mapping.java
+++ src/main/java/com/example/springmvc/controller/Mapping.java
@@ -1,14 +1,24 @@
 package com.example.springmvc.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("map")
+@RequestMapping("mapping")
 public class Mapping {
     @GetMapping
     public String hellow() {
       return "hello hello pakaypo";
+    }
+//    @GetMapping("hello??")
+//    public String hellowMapping() {
+//        return "hello hello pakayooooooo";
+//    }
+    @GetMapping("hello/**")
+    public String hellow1() {
+        return "hello hello pakayooooooo";
+    }
+    @PostMapping("/{name}/{value}")
+    public String hii(@PathVariable("name") String myname,@PathVariable("name") int value){
+        return "path variable is"+myname+"and value is"+value;
     }
 }
