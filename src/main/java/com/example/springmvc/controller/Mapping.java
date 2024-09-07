6bb44e86b636f6523a58842409aa09f18da4a2df
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-07 11:48:36 +0530

    request param

--- src/main/java/com/example/springmvc/controller/Mapping.java
+++ src/main/java/com/example/springmvc/controller/Mapping.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("mapping")
+
 public class Mapping {
     @GetMapping
     public String hellow() {
@@ -18,7 +19,11 @@ public class Mapping {
         return "hello hello pakayooooooo";
     }
     @PostMapping("/{name}/{value}")
-    public String hii(@PathVariable("name") String myname,@PathVariable("name") int value){
+    public String hii(@PathVariable("name") String myname,@PathVariable("value") int value){
         return "path variable is"+myname+"and value is"+value;
+    }
+    @PostMapping(params = {"name","age"})
+    public String hellowMapping(@RequestParam("name")String myname ,@RequestParam("age")int myage){
+        return "my name is "+ myname +" and my age is "+ myage;
     }
 }
